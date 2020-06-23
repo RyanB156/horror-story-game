13384b8951c8d85bb3920719e4a6e0fe230d98c4
RyanB156 <bressetteryan@gmail.com>
2020-06-23 17:50:14 -0400

    Add bootstrap, update routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomMapComponent } from './room-map/room-map.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomMapComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
