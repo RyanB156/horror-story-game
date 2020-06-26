9ca9dba20d594ac5dfce60140e74558b2cf1c374
RyanB156 <bressetteryan@gmail.com>
2020-06-26 18:22:08 -0400

    Turn enums into bit fields

--- src/app/domain/EnumTypes.ts
+++ src/app/domain/EnumTypes.ts
@@ -1,3 +1,3 @@
-export enum Orientation { North, East, South, West }
-export enum Floor { Start = 0, Basement = 1, Ground = 2, Upper = 4 }
+export enum Orientation { North = 1, East = 2, South = 4, West = 8 }
+export enum Floor { Start = 1, Basement = 2, Ground = 4, Upper = 8 }
 export enum CardType { None, Event, Omen, Item }
