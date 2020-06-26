5b93dae50d43927ad6edd1c1bb34997540f24e88
RyanB156 <bressetteryan@gmail.com>
2020-06-26 15:55:48 -0400

    Move floor rendering to FloorComponent and make the view more responsive

--- src/app/room-map/room-map.component.ts
+++ src/app/room-map/room-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, ContentChildren, QueryList, AfterContentInit, ViewChildren } from '@angular/core';
 import { RandomService } from '../random/random.service';
 import { CharacterComponent } from '../character/character.component';
 
@@ -6,6 +6,7 @@ import { Room } from '../domain/Room';
 import { Player } from '../domain/Player';
 import { Floor, CardType, Orientation } from '../domain/EnumTypes';
 import { Pair } from '../domain/Pair';
+import { FloorComponent } from '../floor/floor.component';
 
 /* TODO:
   Set valid doorways for each room. These will need to be set when the room is fixed.
@@ -53,8 +54,8 @@ import { Pair } from '../domain/Pair';
   styleUrls: ['./room-map.component.scss']
 })
 export class RoomMapComponent implements OnInit, AfterViewInit, AfterContentInit {
-  @ViewChild("mapTable") mapTable: ElementRef;
-  @ContentChildren(CharacterComponent) characters: QueryList<CharacterComponent>;
+  
+  @ViewChildren(CharacterComponent) characters: QueryList<CharacterComponent>;
 
   rowCount: number = 10;
   colCount: number = 10;
@@ -68,21 +69,35 @@ export class RoomMapComponent implements OnInit, AfterViewInit, AfterContentInit
   rollNumber: number = 0;
   randomService: RandomService;
 
-  map: Room[][];
+  activeMap: string = "g";
+
+  basementMap: Room[][];
+  groundMap: Room[][];
+  upperMap: Room[][];
 
   rooms: Room[];
+  reachableRooms: Set<Room> = new Set();
 
   constructor(randomService: RandomService) { 
 
     this.randomService = randomService;
-    this.players.push(Player.heatherGranville(1, 2));
-    this.players.push(Player.oxBellows(2, 2));
+    this.players.push(Player.madameZostra());
+    this.players.push(Player.oxBellows());
 
-    this.map = new Array(this.rowCount);
+    this.groundMap = new Array(this.rowCount);
     for (let i = 0; i < this.rowCount; i++) {
-      this.map[i] = new Array(this.colCount);
+      this.groundMap[i] = new Array(this.colCount);
     }
 
+    this.basementMap = new Array(this.rowCount);
+    for (let i = 0; i < this.rowCount; i++) {
+      this.basementMap[i] = new Array(this.colCount);
+    }
+
+    this.upperMap = new Array(this.rowCount);
+    for (let i = 0; i < this.rowCount; i++) {
+      this.upperMap[i] = new Array(this.colCount);
+    }
 
     this.rooms = [
       new Room("Abandoned Room", Floor.Basement | Floor.Ground, CardType.Omen, Room.fourDoors),
@@ -103,13 +118,13 @@ export class RoomMapComponent implements OnInit, AfterViewInit, AfterContentInit
       new Room("Crypt", Floor.Basement, CardType.Event, Room.oneDoor),
       new Room("Dining Room", Floor.Ground, CardType.Omen, Room.oppositeDoors),
       new Room("Dusty Hallway", Floor.Basement | Floor.Ground | Floor.Upper, CardType.None, Room.fourDoors),
-      new Room("Entrance Hall", Floor.Start, CardType.None, Room.threeDoors),
+      new Room("Entrance Hall", Floor.Start, CardType.None, Room.threeDoors, Orientation.East),
       new Room("Foyer", Floor.Start, CardType.None, Room.fourDoors),
       new Room("Furnace Room", Floor.Basement, CardType.Omen, Room.threeDoors),
       new Room("Gallery", Floor.Upper, CardType.Omen, Room.oppositeDoors),
       new Room("Game Room", Floor.Basement | Floor.Ground | Floor.Upper, CardType.Event, Room.threeDoors),
       new Room("Gardens", Floor.Ground, CardType.Event, Room.oppositeDoors),
-      new Room("Grand Staircase", Floor.Start, CardType.None, Room.oppositeDoors, Orientation.East),
+      new Room("Grand Staircase", Floor.Start, CardType.None, Room.oneDoor, Orientation.West),
       new Room("Graveyard", Floor.Ground, CardType.Event, Room.oneDoor),
       new Room("Gymnasium", Floor.Basement | Floor.Upper, CardType.Omen, Room.oppositeDoors),
       new Room("Junk Room", Floor.Basement | Floor.Ground | Floor.Upper, CardType.Omen, Room.fourDoors),
@@ -134,32 +149,61 @@ export class RoomMapComponent implements OnInit, AfterViewInit, AfterContentInit
       new Room("Wine Cellar", Floor.Basement, CardType.Item, Room.oppositeDoors)
     ]
 
-    this.map[3][3] = this.getRoomByName("Basement Landing");
+    // Basement starting rooms
+    this.basementMap[3][3] = this.getRoomByName("Basement Landing");
+    //
 
-    this.map[4][4] = this.getRoomByName("Library");
+    // Ground starting rooms
+    this.groundMap[4][4] = this.getRoomByName("Library");
 
-    this.map[4][5] = this.getRoomByName("Entrance Hall");
-    this.setPlayerPosition(this.players[0], 4, 5);
+    this.groundMap[4][5] = this.getRoomByName("Entrance Hall");
+    this.setPlayerPosition(this.players[0], 4, 5, Floor.Ground);
 
-    this.map[5][5] = this.getRoomByName("Foyer");
-    this.setPlayerPosition(this.players[1], 5, 5);
+    this.groundMap[5][5] = this.getRoomByName("Foyer");
+    this.setPlayerPosition(this.players[1], 5, 5, Floor.Ground);
 
-    this.map[6][5] = this.getRoomByName("Grand Staircase");
-    this.map[7][4] = this.getRoomByName("Upper Landing");
+    this.groundMap[5][6] = this.getRoomByName("Organ Room");
+    this.groundMap[5][7] = this.getRoomByName("Junk Room");
+    
+    this.groundMap[6][7] = this.getRoomByName("Game Room");
+    this.groundMap[6][7].setDirection(Orientation.West);
 
+    this.groundMap[6][6] = this.getRoomByName("Conservatory");
+    this.groundMap[6][6].setDirection(Orientation.South);
+
+    this.groundMap[6][5] = this.getRoomByName("Grand Staircase");
+    //
+
+    // Upper starting rooms
+    this.upperMap[7][4] = this.getRoomByName("Upper Landing");
+    //
+
+    console.log("Calling setReachableRooms");
+    this.setReachableRooms();
   }
 
   ngOnInit(): void {
     
   }
 
-  ngAfterViewInit() : void {
-    this.mapTable.nativeElement.scrollTop = this.mapTable.nativeElement.scrollHeight / 4;
-    this.mapTable.nativeElement.scrollLeft = this.mapTable.nativeElement.scrollHeight / 4;
+  ngAfterViewInit() : void {;
+
   }
 
   ngAfterContentInit() : void {
     
+  }
+
+  seeBasement() : void {
+    this.activeMap = "b";
+  }
+
+  seeGround() : void {
+    this.activeMap = "g";
+  }
+
+  seeUpper() : void {
+    this.activeMap = "u";
   }
 
   statRoll(evt: [number, number]) : void {
@@ -168,10 +212,120 @@ export class RoomMapComponent implements OnInit, AfterViewInit, AfterContentInit
         this.roll();
   }
 
-  setPlayerPosition(player: Player, x: number, y: number) {
-    let room = this.map[x][y];
+  setReachableRooms() : void {
+
+    this.reachableRooms = new Set();
+
+    let player = this.players[this.currentPlayer];
+    let depth = player.speedLevels[player.speed];
+
+    let inner = ((x: number, y: number, depth: number) => {
+
+      console.log(`inner called with (${x}, ${y}, ${depth})`);
+
+      let currentRoom = this.getRoom(x, y);
+
+      console.log("Checking:");
+      console.log(currentRoom);
+
+      if (depth > 0) {
+        // North room.
+
+        console.log("Checking y - 1");
+
+        if (y - 1 >= 0) {
+          let northRoom = this.getRoom(x, y - 1);
+          if (northRoom !== undefined) {
+            console.log(currentRoom);
+            console.log(northRoom);
+
+            // If the player can move North into the next room...
+            if ((currentRoom.openDoors & Orientation.North) > 0 && (northRoom.openDoors & Orientation.South) > 0) {
+              this.reachableRooms.add(northRoom);
+              inner(x, y - 1, depth - 1);
+            }
+          }
+        }
+        // East room.
+
+        console.log("Checking x + 1");
+
+        if (x + 1 < this.colCount) {
+          let eastRoom = this.getRoom(x + 1, y);
+          if (eastRoom !== undefined) {
+            console.log(currentRoom);
+            console.log(eastRoom);
+
+            // If the player can move East into the next room...
+            if ((currentRoom.openDoors & Orientation.East) > 0 && (eastRoom.openDoors & Orientation.West) > 0) {
+              this.reachableRooms.add(eastRoom);
+              inner(x + 1, y, depth - 1);
+            }
+          }
+        }
+        // South room.
+
+        console.log("Checking y + 1");
+
+        if (y + 1 < this.rowCount) {
+          let southRoom = this.getRoom(x, y + 1);
+          if (southRoom !== undefined) {
+            console.log(currentRoom);
+            console.log(southRoom);
+
+            // If the player can move South into the next room...
+            if ((currentRoom.openDoors & Orientation.South) > 0 && (southRoom.openDoors & Orientation.North) > 0) {
+              this.reachableRooms.add(southRoom);
+              inner(x, y + 1, depth - 1);
+            }
+          }
+        }
+        // West room.
+
+        console.log("Checking x - 1");
+
+        if (x - 1 >= 0) {
+          let westRoom = this.getRoom(x - 1, y);
+          if (westRoom !== undefined) {
+            console.log(currentRoom);
+            console.log(westRoom);
+
+            // If the player can move West into the next room...
+            if ((currentRoom.openDoors & Orientation.West) > 0 && (westRoom.openDoors & Orientation.East) > 0) {
+              this.reachableRooms.add(westRoom);
+              inner(x - 1, y, depth - 1);
+            }
+          }
+        }
+      }
+    });
+
+    inner(player.location.x, player.location.y, depth);
+
+    console.log(this.reachableRooms);
+  }
+
+  nextPlayer() : void {
+    this.currentPlayer++;
+    this.currentPlayer %= this.players.length;
+    this.setReachableRooms();
+  }
+
+  setPlayerPosition(player: Player, x: number, y: number, floor: Floor) {
+    
+    var room;
+
+    if (floor === Floor.Basement) {
+      room = this.basementMap[x][y];
+    } else if (floor === Floor.Ground) {
+      room = this.groundMap[x][y];
+    } else {
+      room = this.upperMap[x][y];
+    }
+    
     if (room !== null) {
       player.location = new Pair(x, y);
+      player.floor = floor;
       room.players.push(player);
     } else {
       alert(`Cannot set player at position (${x}, ${y})`);
@@ -184,25 +338,13 @@ export class RoomMapComponent implements OnInit, AfterViewInit, AfterContentInit
 
   /**Get the room from the room map with the specified x and y coordinates. */
   getRoom(i: number, j: number) {
-    return this.map[i][j];
-  }
+    switch (this.activeMap) {
+      case "b": return this.basementMap[i][j];
+      case "g": return this.groundMap[i][j];
+      case "u": return this.upperMap[i][j];
+      default: throw new Error("Invalid floor type");
 
-  /**Get text representing the floor that the room is on. */
-  getFloor(i: number, j: number) {
-    let room: Room = this.getRoom(i, j);
-    let text = "";
-
-    if (room !== null) {
-      if (room.floor === Floor.Start)
-        text += "S";
-      if ((room.floor & Floor.Basement) > 0)
-        text += "B";
-      if ((room.floor & Floor.Ground) > 0)
-        text += "G";
-      if ((room.floor & Floor.Upper) > 0)
-        text += "U";
     }
-    return text;
   }
 
   test() {
